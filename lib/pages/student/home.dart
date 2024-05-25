e7214b717184c71bcf89c821521117eff5af76a9
TanawanUr <143117008+TanawanUr@users.noreply.github.com>
2024-05-26 00:53:41 +0700

    Appbar

--- lib/pages/student/home.dart
+++ lib/pages/student/home.dart
@@ -1,3 +1,5 @@
+// import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,92 +8,88 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(270),
-        child: Container(
-            decoration: BoxDecoration(
-              shape: BoxShape.rectangle,
-              borderRadius: BorderRadius.vertical(
-                bottom: Radius.circular(35),
-              ),
-              gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [
-                  Color.fromARGB(255, 0, 18, 63),
-                  Color.fromARGB(255, 0, 30, 105)
-                ],
-              ),
-            ),
-            child: SafeArea(
-              child: Column(
-                children: [
-                  Container(
-                    child: Padding(
-                      padding: EdgeInsets.fromLTRB(0, 15, 0, 25),
-                      child: Text(
-                        'คะแนนสะสมปัจจุบัน',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 34,
-                          fontWeight: FontWeight.w700,
-                          height: 1,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    child: Padding(
-                      padding: EdgeInsets.fromLTRB(0, 0, 0, 5),
-                      child: Text(
-                        '120',
-                        style: TextStyle(
-                          color: Color(0xffFCCA00),
-                          fontSize: 44,
-                          fontWeight: FontWeight.w800,
-                          height: 1,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Text(
-                    'แต้ม',
-                    style: TextStyle(
-                      color: Color(0xffFCCA00),
-                      fontSize: 32,
-                      fontWeight: FontWeight.w700,
-                      height: 1,
-                    ),
-                  ),
-                  Container(
-                    child: Padding(
-                      padding: EdgeInsets.fromLTRB(0, 23, 0, 7),
-                      child: Text(
-                        'วันหมดอายุคะแนน',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 24,
-                          fontWeight: FontWeight.w700,
-                          height: 1,
-                        ),
-                      ),
-                    ),
-                  ),
+      appBar: appBar(),
+    );
+  }
 
-                  
-                  Text(
-                    '12 กันยายน 2567',
+  PreferredSize appBar() {
+    return PreferredSize(
+      preferredSize: Size.fromHeight(270),
+      child: Container(
+          decoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.vertical(
+              bottom: Radius.circular(35),
+            ),
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Color.fromARGB(255, 0, 18, 63),
+                Color.fromARGB(255, 0, 30, 105)
+              ],
+            ),
+          ),
+          child: SafeArea(
+            child: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.fromLTRB(0, 15, 0, 25),
+                  child: Text(
+                    'คะแนนสะสมปัจจุบัน',
                     style: TextStyle(
                       color: Colors.white,
-                      fontSize: 30,
+                      fontSize: 34,
                       fontWeight: FontWeight.w700,
                       height: 1,
                     ),
                   ),
-                ],
-              ),
-            )),
-      ),
+                ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(0, 0, 0, 5),
+                  child: Text(
+                    '120',
+                    style: TextStyle(
+                      color: Color(0xffFCCA00),
+                      fontSize: 44,
+                      fontWeight: FontWeight.w800,
+                      height: 1,
+                    ),
+                  ),
+                ),
+                Text(
+                  'แต้ม',
+                  style: TextStyle(
+                    color: Color(0xffFCCA00),
+                    fontSize: 32,
+                    fontWeight: FontWeight.w700,
+                    height: 1,
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(0, 23, 0, 7),
+                  child: Text(
+                    'วันหมดอายุคะแนน',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.w700,
+                      height: 1,
+                    ),
+                  ),
+                ),
+                Text(
+                  '12 กันยายน 2567',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.w700,
+                    height: 1,
+                  ),
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
