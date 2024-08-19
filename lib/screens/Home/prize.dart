ee3297c6992c066e88cd43f25f999d0a0a6d6534
TanawanUr <143117008+TanawanUr@users.noreply.github.com>
2024-08-20 01:04:09 +0700

    fix code ##!!!

--- lib/screens/Home/prize.dart
+++ lib/screens/Home/prize.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:system_for_collecting_points_from_plastic_waste/widget/app_buttons.dart';
+import 'package:system_for_collecting_points_from_plastic_waste/screens/PrizeList/Stationery.dart';
 
 class PrizeListPage extends StatelessWidget {
   const PrizeListPage({super.key});
@@ -8,6 +9,7 @@ class PrizeListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xff00154B),
       appBar: AppBar(
         backgroundColor: Color(0xff00154B),
         leading: IconButton(
@@ -26,78 +28,112 @@ class PrizeListPage extends StatelessWidget {
         ),
       ),
       body: Center(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 25,
-            ),
-            AppButtons(
-              onPressed: () {
-                // Navigator.push(
-                //   context,
-                //   MaterialPageRoute(builder: (context) => const PrizeListPage()),
-                // );
-              },
-              textColor: Color(0xff000000),
-              iconColor: Color(0xffEEC004),
-              backgroundColor: Color(0xffFFFFFF),
-              borderColor: Color.fromARGB(44, 0, 0, 0),
-              text: 'แลกอุปกรณ์การเรียน',
-              textSize: 20,
-              iconSize: 80,
-              width: 200,
-              height: 200,
-              blurRadius: 2,
-              icon: "assets/svg/tools.svg",
-            ),
-            SizedBox(
-              height: 25,
-            ),
-            AppButtons(
-              onPressed: () {
-                // Navigator.push(
-                //   context,
-                //   MaterialPageRoute(builder: (context) => const PrizeListPage()),
-                // );
-              },
-              textColor: Color(0xff000000),
-              iconColor: Color(0xffEEC004),
-              backgroundColor: Color(0xffFFFFFF),
-              borderColor: Color.fromARGB(44, 0, 0, 0),
-              text: 'แลกคะแนนจิตพิสัย',
-              textSize: 20,
-              iconSize: 80,
-              width: 200,
-              height: 200,
-              blurRadius: 2,
-              icon: "assets/svg/star.svg",
-            ),
-            SizedBox(
-              height: 25,
-            ),
-            AppButtons(
-              onPressed: () {
-                // Navigator.push(
-                //   context,
-                //   MaterialPageRoute(builder: (context) => const PrizeListPage()),
-                // );
-              },
-              textColor: Color(0xff000000),
-              iconColor: Color(0xffEEC004),
-              backgroundColor: Color(0xffFFFFFF),
-              borderColor: Color.fromARGB(44, 0, 0, 0),
-              text: 'แลกเกียรติบัตร',
-              textSize: 20,
-              iconSize: 80,
-              width: 200,
-              height: 200,
-              blurRadius: 2,
-              icon: "assets/svg/certi.svg",
-            ),
-          ],
+        child: Container(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 10,
+              ),
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Color(0xffEAEAEA),
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(35),
+                    ),
+                  ),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 25,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          AppButtons(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const Stationery()),
+                              );
+                            },
+                            textColor: Color(0xff000000),
+                            iconColor: Color(0xffEEC004),
+                            backgroundColor: Color(0xffFFFFFF),
+                            borderColor: Color.fromARGB(44, 0, 0, 0),
+                            text: 'แลกอุปกรณ์การเรียน',
+                            textSize: 20,
+                            iconSize: 80,
+                            width: 200,
+                            height: 200,
+                            blurRadius: 2,
+                            icon: "assets/svg/tools.svg",
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 25,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          AppButtons(
+                            onPressed: () {
+                              // Navigator.push(
+                              //   context,
+                              //   MaterialPageRoute(builder: (context) => const PrizeListPage()),
+                              // );
+                            },
+                            textColor: Color(0xff000000),
+                            iconColor: Color(0xffEEC004),
+                            backgroundColor: Color(0xffFFFFFF),
+                            borderColor: Color.fromARGB(44, 0, 0, 0),
+                            text: 'แลกคะแนนจิตพิสัย',
+                            textSize: 20,
+                            iconSize: 80,
+                            width: 200,
+                            height: 200,
+                            blurRadius: 2,
+                            icon: "assets/svg/star.svg",
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 25,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          AppButtons(
+                            onPressed: () {
+                              // Navigator.push(
+                              //   context,
+                              //   MaterialPageRoute(builder: (context) => const PrizeListPage()),
+                              // );
+                            },
+                            textColor: Color(0xff000000),
+                            iconColor: Color(0xffEEC004),
+                            backgroundColor: Color(0xffFFFFFF),
+                            borderColor: Color.fromARGB(44, 0, 0, 0),
+                            text: 'แลกเกียรติบัตร',
+                            textSize: 20,
+                            iconSize: 80,
+                            width: 200,
+                            height: 200,
+                            blurRadius: 2,
+                            icon: "assets/svg/certi.svg",
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      backgroundColor: Color(0xffEAEAEA),
     );
   }
 }
