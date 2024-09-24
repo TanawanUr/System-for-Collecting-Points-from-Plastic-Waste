22f297d224e9a334d6021cec2cea567f0975a253
TanawanUr <143117008+TanawanUr@users.noreply.github.com>
2024-09-24 23:02:08 +0700

    add_std_history_list_and_details

---
+++ lib/screens/Student/History/HistoryDetails.dart
@@ -0,0 +1,365 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:system_for_collecting_points_from_plastic_waste/widget/StudentHistory.dart';
+import 'package:intl/intl.dart';
+
+
+class HistoryDetailsPage extends StatefulWidget {
+  // const HistoryDetailsPage({super.key});
+  final StudentHistory item;
+
+  HistoryDetailsPage({required this.item});
+
+  @override
+  State<HistoryDetailsPage> createState() => _HistoryDetailsPageState();
+}
+
+class _HistoryDetailsPageState extends State<HistoryDetailsPage> {
+  late String currentState;
+  late String date;
+  late String submitedDate;
+  late String points;
+  late String itemType;
+  late String itemDescription;
+  late String itemName;
+  late String itemQuantity;
+  late String itemImageUrl;
+  late String reason;
+
+
+
+  @override
+  void initState() {
+    super.initState();
+    itemType = widget.item.type;
+    date = widget.item.date.toString();
+    submitedDate = widget.item.submitedDate.toString();
+    points = widget.item.points.toString();
+    itemDescription = widget.item.description;
+    currentState = widget.item.status;
+    itemName = widget.item.itemName;
+    itemQuantity = widget.item.itemQuantity.toString();
+    itemImageUrl = widget.item.itemImageUrl;
+    reason = widget.item.reason;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff00154B),
+      appBar: AppBar(
+        leading: IconButton(
+          icon: FaIcon(FontAwesomeIcons.angleLeft, color: Colors.white),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        backgroundColor: Color(0xff00154B),
+        surfaceTintColor: Colors.transparent,
+        title: Text(
+          'รายละเอียดเพิ่มเติม',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 34,
+            fontWeight: FontWeight.w700,
+            letterSpacing: -0.5,
+            height: 1,
+          ),
+        ),
+      ),
+      body: Center(
+        child: Container(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 15,
+              ),
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Color(0xffEAEAEA),
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(35),
+                    ),
+                  ),
+                  child: Column(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Color(0xffffffff),
+                          borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(35),
+                          ),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                              left: 20, right: 20, top: 10, bottom: 10),
+                          child: Column(
+                            children: [
+                              _getStatusWidget(submitedDate),
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Container(
+                        width: double.infinity,
+                        color: Colors.white,
+                        child: Column(
+                          children: [
+                            _getTitleWidget(itemType, itemDescription),
+                            Divider(
+                              color: Color(0xffCECECE),
+                              thickness: 1,
+                              height: 1.5,
+                            ),
+                            _getDetailsWidget(date, points, itemType, itemName,
+                                itemQuantity, itemImageUrl,reason),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _getStatusWidget(submitedDate) {
+    switch (currentState) {
+      case 'อนุมัติ':
+        return _buildStatusWidget(
+          title: "คำขอได้รับการอนุมัติ",
+          icon: Icons.check_circle,
+          iconColor: Color(0xff4AAF50),
+          message: formatDateTime(submitedDate),
+        );
+      case 'กำลังรออนุมัติ':
+        return _buildStatusWidget(
+          title: "รอดำเนินการอนุมัติ",
+          icon: Icons.hourglass_empty,
+          iconColor: Color(0xffFCCA00),
+          message: formatDateTime(submitedDate),
+        );
+      case 'ยกเลิก':
+        return _buildStatusWidget(
+          title: "คำขอได้ถูกยกเลิก",
+          icon: Icons.cancel,
+          iconColor: Color(0xffDB3232),
+          message: formatDateTime(submitedDate),
+        );
+      case 'สำเร็จ':
+        return _buildStatusWidget(
+          title: "การดำเนินการสำเร็จ",
+          icon: Icons.check_circle,
+          iconColor: Color(0xff4AAF50),
+          message: formatDateTime(submitedDate),
+        );
+      default:
+        return Text("Unknown State");
+    }
+  }
+
+  Widget _buildStatusWidget(
+      {required String title,
+      IconData? icon,
+      Color? iconColor,
+      required String message}) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: -0.5),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'เมื่อ $message',
+              style: TextStyle(
+                  fontSize: 16, letterSpacing: -0.2, color: Color(0xff464646)),
+            ),
+          ],
+        ),
+        Icon(icon, color: iconColor, size: 60),
+      ],
+    );
+  }
+}
+
+Widget _getDetailsWidget(date, points, itemType, itemName, itemQuantity, itemImageUrl,reason) {
+  return Padding(
+    padding: const EdgeInsets.only(top: 20),
+    child: Column(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(left: 20, right: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 255, 255, 255),
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color(0xff000000).withOpacity(0.1),
+                      spreadRadius: 2,
+                      blurRadius: 2,
+                      offset: Offset(0, 2),
+                    ),
+                  ],
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.network(
+                    itemImageUrl,
+                    // child: Image.asset(
+                    //   'assets/images/pen.png',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 16.0, bottom: 32),
+                  child: Text(
+                    itemName,
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w500,
+                      letterSpacing: -0.5,
+                    ),
+                  ),
+                ),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    'x$itemQuantity',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400,
+                      letterSpacing: -0.5,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text.rich(
+                    TextSpan(
+                      children: [
+                        TextSpan(
+                          text: itemType == 'collect' ? '+ ' : '- ',
+                          style: TextStyle(
+                            color: itemType == 'collect'
+                                ? Colors.green
+                                : Colors.red,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                            letterSpacing: -0.2,
+                          ),
+                        ),
+                        TextSpan(
+                          text: '$points แต้ม',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                            letterSpacing: -0.2,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+          child: Column(
+            children: [
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("ยื่นคำขอเมื่อ",
+                      style: TextStyle(
+                          fontSize: 16,
+                          letterSpacing: -0.2,
+                          color: Color(0xff6C6C6C))),
+                  Text(formatDateTime(date),
+                      style: TextStyle(fontSize: 16, letterSpacing: -0.2)),
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("เหตุผล",
+                      style: TextStyle(
+                          fontSize: 16,
+                          letterSpacing: -0.2,
+                          color: Color(0xff6C6C6C))),
+                  Text(reason,
+                      style: TextStyle(fontSize: 16, letterSpacing: -0.2)),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+String formatDateTime(date) {
+  DateTime parsedDate = DateTime.parse(date); // Parse the string to DateTime
+  String formattedDateTime = DateFormat('dd/MM/yyyy HH:mm').format(parsedDate); // Format to dd/MM/yyyy HH:mm
+  return formattedDateTime;
+}
+String formatSubmitedDate(submitedDate) {
+  DateTime parsedDate = DateTime.parse(submitedDate); // Parse the string to DateTime
+  String formattedDateTime = DateFormat('dd/MM/yyyy HH:mm').format(parsedDate); // Format to dd/MM/yyyy HH:mm
+  return formattedDateTime;
+}
+
+Widget _getTitleWidget(itemType, itemDescription) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+    child: Row(
+      children: <Widget>[
+        SvgPicture.asset(
+          itemType == 'collect'
+              ? 'assets/svg/bottle_icon_black.svg'
+              : 'assets/svg/reward_icon_black.svg',
+          width: 55,
+          height: 55,
+        ),
+        SizedBox(width: 16),
+        Text(itemDescription,
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              letterSpacing: -0.5,
+            )),
+      ],
+    ),
+  );
+}
--- lib/screens/Student/History/history.dart
+++ lib/screens/Student/History/history.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:system_for_collecting_points_from_plastic_waste/widget/app_buttons.dart';
+import 'package:system_for_collecting_points_from_plastic_waste/screens/Student/History/HistoryDetails.dart';
+import 'package:system_for_collecting_points_from_plastic_waste/widget/StudentHistory.dart';
 
 class History_Screen extends StatefulWidget {
   const History_Screen({super.key});
@@ -12,16 +14,81 @@ class History_Screen extends StatefulWidget {
 class _History_ScreenState extends State<History_Screen> {
   @override
   Widget build(BuildContext context) {
+    List<StudentHistory> historyItems = [
+      StudentHistory(
+        type: 'collect',
+        description: 'ใส่ขวด',
+        date: DateTime(2024, 09, 22, 10, 30),
+        submitedDate: DateTime(2021, 10, 1),
+        status: 'สำเร็จ',
+        points: 50,
+        itemName: 'ขวดพลาสติก',
+        itemQuantity: 50,
+        itemImageUrl: 'https://i.ibb.co/R3MC7yQ/plastic-bottle.png',
+        // reason: '',
+      ),
+      StudentHistory(
+        type: 'trade',
+        description: 'แลกของรางวัล',
+        date: DateTime(2024, 09, 01, 10, 30),
+        submitedDate: DateTime(2024, 09, 30),
+        status: 'กำลังรออนุมัติ',
+        points: 20,
+        itemName: 'ปากกา',
+        itemQuantity: 4,
+        itemImageUrl: 'https://i.ibb.co/Stc0T8K/pen.png',
+        // reason: '',
+      ),
+      StudentHistory(
+        type: 'trade',
+        description: 'แลกของรางวัล',
+        date: DateTime(2024, 08, 30, 10, 30),
+        submitedDate: DateTime(2024, 01, 18),
+        status: 'อนุมัติ',
+        points: 50,
+        itemName: 'ยางลบ',
+        itemQuantity: 5,
+        itemImageUrl: 'https://i.ibb.co/vDgzVvM/eraser.png',
+        // reason: '',
+      ),
+      StudentHistory(
+        type: 'collect',
+        description: 'ใส่ขวด',
+        date: DateTime(2024, 07, 14, 10, 30),
+        submitedDate: DateTime(2024, 06, 14, 10, 30),
+        status: 'สำเร็จ',
+        points: 10,
+        itemName: 'ขวดพลาสติก',
+        itemQuantity: 10,
+        itemImageUrl: 'https://i.ibb.co/R3MC7yQ/plastic-bottle.png',
+        // reason: '',
+      ),
+      StudentHistory(
+        type: 'trade',
+        description: 'แลกของรางวัล',
+        date: DateTime(2024, 05, 27, 10, 30),
+        submitedDate: DateTime(2025, 01, 21),
+        status: 'ยกเลิก',
+        points: 30,
+        itemName: 'ดินสอ',
+        itemQuantity: 1,
+        itemImageUrl: 'https://i.ibb.co/G00RtM9/pencil.png',
+        reason: 'ของรางวัลหมด',
+      ),
+    ];
+
     return Scaffold(
       backgroundColor: Color(0xff00154B),
       appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 0, 21, 75),
+        backgroundColor: Color(0xff00154B),
+        surfaceTintColor: Colors.transparent,
         title: Text(
           'ประวัติการใช้งาน',
           style: TextStyle(
             color: Colors.white,
-            fontSize: 32,
+            fontSize: 34,
             fontWeight: FontWeight.w700,
+            letterSpacing: -0.5,
             height: 1,
           ),
         ),
@@ -43,33 +110,148 @@ class _History_ScreenState extends State<History_Screen> {
                   ),
                   child: Column(
                     children: [
-                      SizedBox(
-                        height: 15,
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Color(0xffffffff),
+                          borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(35),
+                          ),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                              left: 20, right: 20, top: 10, bottom: 10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text("รายการล่าสุด",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 26,
+                                      fontWeight: FontWeight.w700,
+                                      letterSpacing: -0.5)),
+                              // IconButton(
+                              //   onPressed: () {},
+                              //   icon: FaIcon(
+                              //     FontAwesomeIcons.arrowDownShortWide,
+                              //     size: 25,
+                              //     color: Colors.black,
+                              //   ),
+                              // ),
+                            ],
+                          ),
+                        ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 20,right: 20),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            
-                            Text("รายการล่าสุด",
-                                style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 26,
-                                  fontWeight: FontWeight.w700,
-                                )
-                              ),
-                            IconButton(
-                              onPressed: (){
-                        
-                              }, 
-                              icon: FaIcon(
-                                FontAwesomeIcons.arrowDownShortWide,
-                                size: 25,
-                                 color: Colors.black,
-                              ),
-                            ),
-                          ],
+                      Divider(
+                        color: Color(0xffEAEAEA),
+                        thickness: 1,
+                        height: 1.5,
+                      ),
+                      Expanded(
+                        child: ListView.builder(
+                          itemCount: historyItems.length,
+                          itemBuilder: (context, index) {
+                            final item = historyItems[index];
+                            return Column(
+                              children: [
+                                Container(
+                                  color: Colors.white,
+                                  child: ListTile(
+                                    leading: SvgPicture.asset(
+                                      item.type == 'collect'
+                                          ? 'assets/svg/bottle_icon.svg'
+                                          : 'assets/svg/reward_icon.svg',
+                                      width: 55,
+                                      height: 55,
+                                    ),
+                                    title: Text(item.description,
+                                        style: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.w700,
+                                            letterSpacing: -0.5)),
+                                    subtitle: Container(
+                                      padding: const EdgeInsets.only(top: 8),
+                                      child: Text(_formatTime(item.date),
+                                          style: TextStyle(
+                                              color: Color(0xff136BFF),
+                                              fontSize: 13,
+                                              fontWeight: FontWeight.w400,
+                                              letterSpacing: -0.2)),
+                                    ),
+                                    trailing: Padding(
+                                      padding: const EdgeInsets.only(top: 5),
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.end,
+                                        children: [
+                                          if (item.type == 'trade')
+                                            Text(item.status,
+                                                style: TextStyle(
+                                                    color: _getStatusColor(
+                                                        item.status),
+                                                    fontSize: 14,
+                                                    fontWeight: FontWeight.w500,
+                                                    letterSpacing: -0.2)),
+                                          if (item.type == 'collect')
+                                            Text.rich(
+                                              TextSpan(
+                                                children: [
+                                                  TextSpan(
+                                                    text: '+ ',
+                                                    style: TextStyle(
+                                                      color: Colors
+                                                          .green, // Set the '+' to green
+                                                      fontSize: 14,
+                                                      fontWeight:
+                                                          FontWeight.w500,
+                                                      letterSpacing: -0.2,
+                                                    ),
+                                                  ),
+                                                  TextSpan(
+                                                    text: '${item.points} แต้ม',
+                                                    style: TextStyle(
+                                                      color: _getStatusColor(item
+                                                          .status), // Color based on status
+                                                      fontSize: 14,
+                                                      fontWeight:
+                                                          FontWeight.w500,
+                                                      letterSpacing: -0.2,
+                                                    ),
+                                                  ),
+                                                ],
+                                              ),
+                                            ),
+                                          Text('แสดงเพิ่มเติม',
+                                              style: TextStyle(
+                                                  fontSize: 13,
+                                                  color: Color(0xff136BFF),
+                                                  fontWeight: FontWeight.w400,
+                                                  letterSpacing: -0.2)),
+                                        ],
+                                      ),
+                                    ),
+                                    onTap: () {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (context) => HistoryDetailsPage(
+                                              item:
+                                                  item), // Passing the item data to the next page
+                                        ),
+                                      );
+                                    },
+                                  ),
+                                ),
+                                Divider(
+                                  color: Color(0xffEAEAEA),
+                                  thickness: 1,
+                                  height: 1.5,
+                                ),
+                              ],
+                            );
+                          },
                         ),
                       ),
                     ],
@@ -120,5 +302,33 @@ class _History_ScreenState extends State<History_Screen> {
             ),
           )),
     );
+  }
+
+  String _formatTime(DateTime date) {
+    final now = DateTime.now();
+    final difference = now.difference(date);
+
+    if (difference.inSeconds < 60) {
+      return '${difference.inSeconds} วินาทีที่แล้ว';
+    } else if (difference.inMinutes < 60) {
+      return '${difference.inMinutes} นาทีที่แล้ว';
+    } else if (difference.inHours < 24) {
+      return '${difference.inHours} ชั่วโมงที่แล้ว';
+    } else {
+      return '${difference.inDays} วันที่แล้ว';
+    }
+  }
+
+  Color _getStatusColor(String status) {
+    switch (status.toLowerCase()) {
+      case 'อนุมัติ':
+        return Color(0xff4AAF50);
+      case 'กำลังรออนุมัติ':
+        return Color(0xffFCCA00);
+      case 'ยกเลิก':
+        return Color(0xffDB3232);
+      default:
+        return Colors.black;
+    }
   }
 }
