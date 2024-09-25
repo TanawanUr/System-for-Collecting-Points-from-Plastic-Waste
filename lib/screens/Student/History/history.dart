10b952851a27eb14591ccb34dacb826ba2c8ae42
TanawanUr <143117008+TanawanUr@users.noreply.github.com>
2024-09-25 23:26:05 +0700

    delete_import_package

--- lib/screens/Student/History/history.dart
+++ lib/screens/Student/History/history.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:system_for_collecting_points_from_plastic_waste/screens/Student/History/HistoryDetails.dart';
 import 'package:system_for_collecting_points_from_plastic_waste/widget/StudentHistory.dart';
 
