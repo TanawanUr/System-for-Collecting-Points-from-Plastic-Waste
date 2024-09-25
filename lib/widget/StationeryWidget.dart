ec48a250694596ddf3eddf0cab8e44e4e1b9fdf9
TanawanUr <143117008+TanawanUr@users.noreply.github.com>
2024-09-25 23:28:01 +0700

    create_screen_and_widget_stationery

---
+++ lib/widget/StationeryWidget.dart
@@ -0,0 +1,13 @@
+class StationeryWidget {
+  final int points;
+  final String itemName;
+  final int itemQuantity;
+  final String itemImageUrl;
+
+  StationeryWidget({
+    this.points = 0, 
+    required this.itemName,
+    required this.itemQuantity,
+    required this.itemImageUrl,
+  });
+}
